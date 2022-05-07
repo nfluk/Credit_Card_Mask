39af31d559f65533e9ed29953fb5e81bb3a1602c
nfluk <nickylukic@gmail.com>
2022-05-07 14:34:02 +0200

    put all inside if to check for if length > 4

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 function maskify(cc) {
   let maskedString = '';
-  const stringToMask = cc.length - 4;
-  const endString = cc.slice(stringToMask);
 
-  for (let i = 0; i < stringToMask; i++) {
-    maskedString += '#';
+  if (cc.length > 4) {
+    const stringToMask = cc.length - 4;
+    const endString = cc.slice(stringToMask);
+
+    for (let i = 0; i < stringToMask; i++) {
+      maskedString += '#';
+    }
+    return maskedString + endString;
   }
-  return maskedString + endString;
+
+  return cc;
 }
